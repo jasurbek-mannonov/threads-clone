aa1083c7d6febc5db2419d4431f7b2e06d7f7e90
jasurbek-mannonov <jasurbekmannonov@gmail.com>
2023-12-09 18:44:27 +0500

    created auth page and modified

---
+++ pages/auth.vue
@@ -0,0 +1,16 @@
+<script setup>
+// const client = useSupabaseClient()
+// const user = useSupabaseUser()
+
+watchEffect(() => {
+    if(user.value){
+        return navigateTo('/')
+    }
+})
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
