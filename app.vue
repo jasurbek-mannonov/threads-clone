0a99f7b0de034f44ab4b6f9350e64f17f3c28105
jasurbek-mannonov <jasurbekmannonov@gmail.com>
2023-12-09 19:26:48 +0500

    Completed Modal component

--- app.vue
+++ app.vue
@@ -11,6 +11,13 @@ const userStore = useUserStore()
     <CreatePost
       :class="[
         {'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isMenuOverlay},
+        {'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isMenuOverlay}
+      ]"
+    />
+
+    <Modal
+      :class="[
+        {'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isLogoutOverlay},
         {'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isLogoutOverlay}
       ]"
     />
