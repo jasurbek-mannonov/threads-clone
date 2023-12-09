24ad1d4bb01434c83123979fb5b6560968181567
jasurbek-mannonov <jasurbekmannonov@gmail.com>
2023-12-09 17:43:28 +0500

    changes

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import MainLayout from '~/layouts/MainLayout.vue';
+
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore()
+// const user = useSupabaseUser()
+
+let posts = ref([])
+let isPosts = ref(true)
+let isLoading = ref(false)
+
+onBeforeMount(() => {
+    posts.value = [
+        {
+            name: "Jasurbek Mannonov VUE developer",
+            image: "https://placehold.co/100",
+            text: "This is the Title",
+            picture: "https://placehold.co/500"
+        }
+    ]
+})
+</script>
+
+<template>
+    <MainLayout>
+        <div id="IndexPage" class="w-full overflow-auto">
+            <div class="mx-auto max-w-[500px] overflow-hidden">
+                <div id="Posts" class="px-4 max-w-[600px] mx-auto">
+                    <div class="text-white" v-if="isPosts" v-for="post in posts" :key="post">
+                        <Post :post="post" isDeleted="posts = []"/> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </MainLayout>
+</template>
